736b0b3d9b0a3f975fd55effe1bb5adc10890e06
guilherme <guilhermemarzano@gmail.com>
2024-01-13 19:44:10 -0300

    Alteracao estrutura de pasta

--- Aula_00_revisao_html_python/ExClasse/cadastro_pessoa.py
+++
@@ -1,6 +0,0 @@
-from cls_Pessoa import Pessoa
-
-flg_app = True
-
-pessoas = []
-
---
+++ Aula_00_revisao_html_python/Python_rev/ExClasse/cadastro_pessoa.py
@@ -0,0 +1,6 @@
+from Aula_00_revisao_html_python.Python_rev.ExClasse.ExClasse.cls_Pessoa import Pessoa
+
+flg_app = True
+
+pessoas = []
+
