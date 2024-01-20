bdbdd11c30efc7c8f01293dac1df5843446e9519
guilherme <guilhermemarzano@gmail.com>
2024-01-20 18:56:22 -0300

    end firs part

--- Aula_01_introducao_flask/app.py
+++ Aula_01_introducao_flask/app.py
@@ -2,11 +2,55 @@ from flask import Flask
 
 #criando nossa aplicação flask
 app = Flask(__name__)
+#Para inpedir o app de order as chaves do dicionario
+app.json.sort_keys = False
 
 #Aqui estamos criando uma rota
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World'
+pessoas:list[dict] = [
+    {
+        "id":1,
+        "nome":"Davi",
+        "idade":30,
+        "altura":1.7,
+        "habilidades":["Python","flask","programação geral"]
+    },
+    {
+        "id":2,
+        "nome":"Gui",
+        "idade":40,
+        "altura":1.67,
+        "habilidades":["Python","C#","alteryx","tableau"]
+    },
+    {
+        "id":3,
+        "nome":"Bi",
+        "idade":40,
+        "altura":1.65,
+        "habilidades":["Empata","Sensitiva","gerenciamento"]
+    },
+]
+
+# @app.route('/pessoas')
+# def listar_pessoas():
+#     return pessoas
+
+#---------------------------
+#Boas praticas informar quais metodos que a rota acita:
+@app.route('/pessoas',methods = ['GET'])
+def listar_pessoas():
+    return pessoas
+
+#Metodo para detelhar pessoas
+#Isso chama path param - quando o parametro vem pela URL.
+@app.route('/pessoas/<int:id>',methods = ['GET'])
+def detalhar_pessoas(id:int):
+    for pessoa in pessoas:
+        if pessoa.get('id') == id:
+            return pessoa
+    return ({'message': 'Pessoa não encontrada'}, 404)
 
 #Aqui estamos executando a aplicação
 
